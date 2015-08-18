7c760c9f514602149fba5179bdc93df60f6ed718
David Longenecker <github@7longeneckers.com>
2015-08-18 11:43:10 -0500

    Rev 0.3 - offline analysis
    Rev 0.3 adds support for offline registry files. Rev 0.2 (live analysis
    only) is available in the /v0.2 folder.

--- reglister.py
+++ reglister.py
@@ -1,15 +1,24 @@
-# RegLister v0.2
+# RegLister v0.3
 # Source: https://github.com/dnlongen/RegLister
 # Author: David Longenecker
 # Author email: david@securityforrealpeople.com 
 # Author Twitter: @dnlongen
 # Explanation: http://securityforrealpeople.com/reglister
+# Hat-tip to @patrickrolsen for example code that helped with parsing offline files
 
 import winreg,sys,argparse
 
+try:
+  offline=True
+  from Registry import Registry
+except:
+  offline=False
+
 # Define suported parameters and defaut values
 parser = argparse.ArgumentParser(description='Recursively scan a Windows registry and print keys and values with a large data content. Hiding executable files in the registry is a common malware technique; as such files tend to be larger than most normal registry data, RegLister helps locate potentially suspicious registry data.')
-parser.add_argument('-c', '--computername', dest='computername', default='', help='Remote computername to connect; if not specified, the local registry will be used')
+parser_ex = parser.add_mutually_exclusive_group(required=False)
+parser_ex.add_argument('-c', '--computername', dest='computername', default='', help='Remote computername to connect; if not specified, the local registry will be used')
+parser_ex.add_argument('-f', '--filename', default='', help='Specify a registry hive filename to load for offline analysis. Note: --computername and --filename are mutually exclusive')
 parser.add_argument('-m', '--minsize', default=20, type=int, help='Show all data larger than this; default 20KB')
 parser.add_argument('-v', '--verbose', dest='verbose', action='store_true', help='Display verbose error messages; this will show errors for registry values to which you do not have access')
 parser.add_argument('-d', '--debug', dest='debug', action='store_true')
@@ -19,11 +28,12 @@ minsize=(args.minsize*1024)
 verbose=args.verbose
 computername=args.computername
 if computername: computername = "\\\\" + computername
+regfile=args.filename
 debug=args.debug
 
 hives = ["HKEY_CURRENT_USER","HKEY_LOCAL_MACHINE","HKEY_USERS","HKEY_CLASSES_ROOT","HKEY_CURRENT_CONFIG"]
 
-# Anything in the whitelist will be ignored, ragardless of the size of data content.
+# Anything in the whitelist will be ignored, regardless of the size of data content.
 whitelist = [
   "HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\PushNotifications\AppDB",
   "HKEY_CURRENT_USER\Software\Classes\Local Settings\Software\Microsoft\Windows\CurrentVersion\TrayNotify\IconStreams",
@@ -55,6 +65,9 @@ def ListValues(path,key):
   return
 
 def ListKeys(path,key):
+  # ListKeys will enumerate the *subkeys* in a specific registry key or hive. For each it will first call ListValues
+  # to process the data values in the subkey, then call ListKeys to check for any further subkeys beneath the
+  # current subkey
   if debug: print("ListKeys: " + path)
   i = 0
   subkey = ""
@@ -75,28 +88,78 @@ def ListKeys(path,key):
     pass
   return
 
+def getOfflineEntries(reg):
+  for values in reg.values():
+    try:
+      binsize = sys.getsizeof(values.value())
+      if debug: 
+        fullpath = regfile + "->" + reg.path() + "\\" + values.name()
+        print(fullpath + ": " + str(binsize))
+      if binsize >= minsize:
+        fullpath = regfile + "->" + reg.path() + "\\" + values.name()
+        if values.value_type() == Registry.RegBin:
+          value = values.value()[:128]
+          print(fullpath + ": " + str(binsize))
+        else:
+          value = values.value()
+          print (fullpath + ": " + str(binsize))
+    except TypeError as e:
+      if (debug or verbose): print("TypeError handling subkey " + reg.path() + "\\" + values.name())
+      pass
+    except:
+      e = sys.exc_info()[0]
+      if (debug or verbose): print("Error handling subkey " + reg.path() + "\\" + values.name())
+      if (debug or verbose): print("Error message: " + str(e))
+      pass
+  for subkey in reg.subkeys():
+    if debug: print("opening " + reg.path())
+    getOfflineEntries(subkey)
+
 if __name__ == '__main__':
   if debug:
     print("Starting RegLister")
     print("Minsize: " + str(minsize))
     print("Computername: " + computername)
-  for hive in hives:
-    # Try this which each valid registry hive
-    try:
-      if debug: 
-        if computername: print("Processing hive " + computername + "\\" + hive)
-        else: print("Processing local registry hive " + hive)
-      registry=winreg.ConnectRegistry(r"%s" % computername,getattr(winreg, hive))
-      ListKeys(hive, winreg.OpenKey(registry,""))
-    except OSError as e:
-      if computername: print("Error opening " + computername + "\\" + hive)
-      else: print("Error opening local registry hive " + hive)
-      print("Error code: " + str(e.errno) + " (" + e.strerror + ")")
-      if ((verbose or debug) and (e.errno==2 or e.errno==53)): 
-        print("Perhaps you need to map a null session?")
-        print("Is the remote registry service enabled on " + computername + "?")
-        print("Is Windows firewall enabled on " + computername + "?")
+    print("Filename: " + regfile)
+  if regfile:
+    if offline:
+      if (debug or verbose): print("opening registry file " + regfile)
+      try:
+        reg = Registry.Registry(regfile).root()
+      except FileNotFoundError:
+        print("Error opening local registry file " + regfile)
+        print("FileNotFoundError can indicate the file is in use and locked by the system")
         sys.exit()
-      if ((verbose or debug) and e.errno==13): 
-        print("Perhaps you need to map a null session?")
-        pass
+      except:
+        e = sys.exc_info()[0]
+        print("Error opening local registry file " + regfile)
+        print("Error message: " + str(e))
+        sys.exit()
+      getOfflineEntries(reg)
+    else:
+      #Registry.Registry module was not imported, so offline analysis is not available
+      print("Offline analysis requires the Registry.Registry module, part of python-registry.")
+      print("This may be found at https://github.com/williballenthin/python-registry")
+      print("Only live registry analysis is available without this module.")
+  else:
+    if (debug or verbose): print("Processing live registry")
+    for hive in hives:
+      # Try this which each valid registry hive
+      try:
+        if debug: 
+          if computername: print("Processing hive " + computername + "\\" + hive)
+          else: print("Processing local registry hive " + hive)
+        registry=winreg.ConnectRegistry(r"%s" % computername,getattr(winreg, hive))
+        ListKeys(hive, winreg.OpenKey(registry,""))
+      except OSError as e:
+        if computername: print("Error opening " + computername + "\\" + hive)
+        else: print("Error opening local registry hive " + hive)
+        print("Error code: " + str(e.errno) + " (" + e.strerror + ")")
+        if ((verbose or debug) and (e.errno==2 or e.errno==53)): 
+          print("Perhaps you need to map a null session?")
+          print("Is the remote registry service enabled on " + computername + "?")
+          print("Is Windows firewall enabled on " + computername + "?")
+          sys.exit()
+        if ((verbose or debug) and e.errno==13): 
+          print("Perhaps you need to map a null session?")
+          pass
---
+++ v0.1/reglister.py
@@ -0,0 +1,65 @@
+# RegLister v0.1
+# Source: https://github.com/dnlongen/RegLister
+# Author: David Longenecker (Twitter: @dnlongen)
+
+import winreg,sys,argparse
+
+parser = argparse.ArgumentParser(description='Recursively scan a Windows registry and print keys and values with a large data content. Hiding executable files in the registry is a common malware technique; as such files tend to be larger than most normal registry data, RegLister helps locate potentially suspicious registry data.')
+parser.add_argument('--minsize', default=20, type=int, help='Show all data larger than this; default 20KB')
+parser.add_argument('-v', '--verbose', dest='verbose', action='store_true', help='Display verbose error messages; this will show errors for registry values to which you do not have access')
+parser.set_defaults(verbose=False)
+args=parser.parse_args()
+minsize=(args.minsize*1024)
+verbose=args.verbose
+
+debug=False
+#minsize = 20480 # default to 20 KB
+hives = ["HKEY_CURRENT_USER","HKEY_LOCAL_MACHINE","HKEY_USERS","HKEY_CLASSES_ROOT","HKEY_CURRENT_CONFIG"]
+whitelist = [
+  "HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\PushNotifications\AppDB",
+  "HKEY_CURRENT_USER\Software\Classes\Local Settings\Software\Microsoft\Windows\CurrentVersion\TrayNotify\IconStreams",
+  "HKEY_CURRENT_USER\Software\Classes\Local Settings\Software\Microsoft\Windows\CurrentVersion\TrayNotify\PastIconsStream",
+  "HKEY_CLASSES_ROOT\Local Settings\Software\Microsoft\Windows\CurrentVersion\TrayNotify\IconStreams",
+  "HKEY_CLASSES_ROOT\Local Settings\Software\Microsoft\Windows\CurrentVersion\TrayNotify\PastIconsStream"
+]
+
+def ListValues(path,key):
+  if debug: print("ListValues: " + path)
+  i = 0
+  value = ""
+  try:
+    while 1:
+      value = (winreg.EnumValue(key,i))
+      if debug: print(path + "\\" + value[0] + " ("+ str(value[1]) + ") " + str(sys.getsizeof(value[1])))
+      if not (path + "\\" + value[0]) in whitelist:
+        if sys.getsizeof(value[1]) > minsize:
+          print(path + "\\" + value[0] + ": " + str(sys.getsizeof(value[1])))
+      i += 1
+  except WindowsError as e:
+    if not (e.errno == 22):
+      if verbose: print("Error opening " + path + "[" + i + "]; " + str(e.errno) + ": " + e.strerror)
+    pass
+  return
+
+def ListKeys(path,key):
+  if debug: print("ListKeys: " + path)
+  i = 0
+  subkey = ""
+  try:
+    while 1:
+      subkey = winreg.EnumKey(key,i)
+      if debug: print("subkey: " + subkey)
+      ListValues(path + "\\" + subkey, winreg.OpenKey(key,subkey))
+      if debug: print("Calling ListKeys with " + path +"\\" + subkey)
+      ListKeys(path + "\\" + subkey, winreg.OpenKey(key,subkey))
+      i += 1
+  except WindowsError as e:
+    if not (e.errno == 22):
+      if verbose: print("Error opening " + path + "\\" + subkey + "; " + str(e.errno) + ": " + e.strerror)
+    pass
+  return
+
+if __name__ == '__main__':
+  for hive in hives:
+    if debug: print("Processing hive " + hive)
+    ListKeys(hive, winreg.OpenKey(getattr(winreg, hive),""))
